962b7946d526bada5681ac31946a9654b10e9ee3
Mazin Mustafa <mazin.mustafa@okstate.edu>
2025-07-02 13:50:24 -0500

    update

--- include/transmission_line.h
+++ include/transmission_line.h
@@ -3,9 +3,21 @@
 
 // Definitions
 #include "basic_definitions.h"
+#include "layers.h"
+
 typedef enum sheet_t{sheet_I, sheet_II, sheet_III, sheet_IV}sheet_t;
+typedef struct spectral_parameters_t{
+    complex_t k_z, Theta, Z_e, Z_h;
+}spectral_parameters_t;
+typedef struct TL_parameters_t{
+    complex_t Gamma_ij_e, Gamma_ij_h;
+    complex_t tau_ij_e, tau_ij_h;
+    complex_t Omega_ij_e, Omega_ij_h;
+}TL_parameters_t;
 
 // Functions
 complex_t sqrt_Riemann(const complex_t z, const sheet_t sheet);
+spectral_parameters_t spectral_components(const complex_t k_rho, config_t *config, 
+	const size_t n, const sheet_t sheet);
 
 #endif
--- src/transmision_line.c
+++ src/transmision_line.c
@@ -12,10 +12,66 @@ complex_t sqrt_Riemann(const complex_t z, const sheet_t sheet){
 	return 0.0;
 }
 
-void spectral_components(){
-
+spectral_parameters_t spectral_components(const complex_t k_rho, config_t *config, 
+	const size_t n, const sheet_t sheet){
+	assert_error(n<config->N, "invalid layer number");
+	spectral_parameters_t spectral;
+	const complex_t k=config->layers[n].k;
+	const real_t d=config->layers[n].d;
+	const size_t N=config->N;
+	const real_t omega=config->omega;
+	const complex_t mu=config->layers[n].mu; 
+	const complex_t eps=config->layers[n].eps;
+	complex_t k_z=0.0;
+	if (sheet==sheet_I){
+		if (n==0){
+			k_z = sqrt_Riemann(k*k-k_rho*k_rho, sheet_I);
+		}
+		if (n==(N-1)){
+			k_z = sqrt_Riemann(k*k-k_rho*k_rho, sheet_I);
+		}
+	}else
+	if (sheet==sheet_II){
+		if (n==0){
+			k_z = sqrt_Riemann(k*k-k_rho*k_rho, sheet_II);
+		}
+		if (n==(N-1)){
+			k_z = sqrt_Riemann(k*k-k_rho*k_rho, sheet_I);
+		}
+	}else
+	if (sheet==sheet_III){
+		if (n==0){
+			k_z = sqrt_Riemann(k*k-k_rho*k_rho, sheet_I);
+		}
+		if (n==(N-1)){
+			k_z = sqrt_Riemann(k*k-k_rho*k_rho, sheet_II);
+		}
+	}else
+	if (sheet==sheet_IV){
+		if (n==0){
+			k_z = sqrt_Riemann(k*k-k_rho*k_rho, sheet_II);
+		}
+		if (n==(N-1)){
+			k_z = sqrt_Riemann(k*k-k_rho*k_rho, sheet_II);
+		}
+	}else{
+		k_z = sqrt_Riemann(k*k-k_rho*k_rho, sheet_I);
+	} 
+	spectral.k_z = k_z;
+	spectral.Theta = k_z*d;
+	spectral.Z_e = k_z/(omega*_eps_0*eps);
+	spectral.Z_h = (omega*_mu_0*mu)/k_z;
+	return spectral;
 }
 
-// complex_t gamma_TL(){
-
-// }
+TL_parameters_t basic_reflection_TL(const complex_t k_rho, config_t *config, const size_t i, const size_t j, 
+	const sheet_t sheet){
+	TL_parameters_t parameters; 
+	spectral_parameters_t para_i=spectral_components(k_rho, config, i, sheet);
+	spectral_parameters_t para_j=spectral_components(k_rho, config, j, sheet);
+	parameters.Gamma_ij_e = (para_i.Z_e-para_j.Z_e)/(para_i.Z_e+para_j.Z_e);
+	parameters.Gamma_ij_h = (para_i.Z_h-para_j.Z_h)/(para_i.Z_h+para_j.Z_h);
+	parameters.tau_ij_e = 1.0+parameters.Gamma_ij_e;
+	parameters.tau_ij_h = 1.0+parameters.Gamma_ij_h;
+	return parameters;
+}
