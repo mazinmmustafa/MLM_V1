be4cb63d9e5a953342d25f4f84b60277b6cf3b75
Mazin M. Mustafa <mazin.mustafa@outlook.com>
2025-07-04 21:56:11 -0500

    update

--- include/testbench_basic.hpp
+++ include/testbench_basic.hpp
@@ -6,22 +6,14 @@
 #include "utilities.hpp"
 #include "special_functions.hpp"
 #include "matrix.hpp"
-#include "mesh.hpp"
 #include "math_utilities.hpp"
 #include "quadl.hpp"
-#include "shape.hpp"
-#include "projection.hpp"
-#include "engine_1d.hpp"
 
 // Definitions
 
 // Functions
 void test_bessel();
 void test_matrix();
-void test_mesh();
 void test_quadl();
-void test_shape();
-void test_engine_projection_1d();
-void test_engine_1d();
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,9 @@
 
 int main(){
 
-    // test_bessel();
-    // test_matrix();
-    // test_mesh();
-    // test_quadl();
-    // test_shape();
-    // test_engine_projection_1d();
-    test_engine_1d();
+    test_bessel();
+    test_matrix();
+    test_quadl();
 
     return 0;
 }
