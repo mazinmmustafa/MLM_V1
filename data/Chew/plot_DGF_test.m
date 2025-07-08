3bcb29a69f50f48527e6f6afbf67eaea8bb19b8f
Mazin Mustafa <mazin.mustafa@okstate.edu>
2025-07-08 16:06:55 -0500

    update

--- data/Chew/plot_DGF_test.m
+++ data/Chew/plot_DGF_test.m
@@ -1,72 +1,90 @@
 close all; clear; clc;
 
-data = load('data_EJ.txt');
+n = 1;
 
-figure()
-hold on
-plot(data(:, 1), data(:, 2), '-')
-hold off
+if n==1
+  
+  data = load('data_EJ.txt');
 
-figure()
-hold on
-plot(data(:, 1), data(:, 3), '-')
-hold off
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 2), '-')
+  hold off
 
-figure()
-hold on
-plot(data(:, 1), data(:, 4), '-')
-hold off
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 3), '-')
+  hold off
 
-data = load('data_EM.txt');
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 4), '-')
+  hold off
 
-figure()
-hold on
-plot(data(:, 1), data(:, 2), '-')
-hold off
+end
 
-figure()
-hold on
-plot(data(:, 1), data(:, 3), '-')
-hold off
+if n==2
+  
+  data = load('data_EM.txt');
 
-figure()
-hold on
-plot(data(:, 1), data(:, 4), '-')
-hold off
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 2), '-')
+  hold off
 
-data = load('data_HJ.txt');
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 3), '-')
+  hold off
 
-figure()
-hold on
-plot(data(:, 1), data(:, 2), '-')
-hold off
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 4), '-')
+  hold off
 
-figure()
-hold on
-plot(data(:, 1), data(:, 3), '-')
-hold off
+end
 
-figure()
-hold on
-plot(data(:, 1), data(:, 4), '-')
-hold off
+if n==3
+  data = load('data_HJ.txt');
 
-data = load('data_HM.txt');
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 2), '-')
+  hold off
 
-figure()
-hold on
-plot(data(:, 1), data(:, 2), '-')
-hold off
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 3), '-')
+  hold off
 
-figure()
-hold on
-plot(data(:, 1), data(:, 3), '-')
-hold off
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 4), '-')
+  hold off
 
-figure()
-hold on
-plot(data(:, 1), data(:, 4), '-')
-hold off
+end
+
+if n==4
+  
+  data = load('data_HM.txt');
+
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 2), '-')
+  hold off
+
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 3), '-')
+  hold off
+
+  figure()
+  hold on
+  plot(data(:, 1), data(:, 4), '-')
+  hold off
+  
+end
+
 
 
 
