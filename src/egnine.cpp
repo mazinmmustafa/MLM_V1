8411aa259042c5d29ff54c0d4b59616fe5b80e54
Mazin M. Mustafa <mazin.mustafa@outlook.com>
2025-07-12 22:25:59 -0500

    update

--- src/egnine.cpp
+++ src/egnine.cpp
@@ -1605,14 +1605,15 @@ far_field_t configuration_t::compute_far_field_J(const real_t r, const real_t th
         vector_t<complex_t> J=vector_t<complex_t>(dipole_J.x, dipole_J.y, dipole_J.z);
         const complex_t factor=exp(+j*(vector_k_rho_s.x*dipole_J.r.x+vector_k_rho_s.y*dipole_J.r.y));
         vector_t<complex_t> E;
-        fields.E.theta = -j*k_1*(exp(-j*k_1*r)/(2.0*pi*r))*exp(+j*k_1*cos(theta_s)*z_)*(
-            TLGF_i.V_e*(vector_rho_s*J)-TLGF_v.V_e*eta_1*(eps_1/eps_)*sin(theta_s)*(vector_z_s*J)
+        fields.E.theta = -j*2.0*k_1*(exp(-j*k_1*r)/(4.0*pi*r))*exp(+j*k_1*cos(theta_s)*z_)*(
+            TLGF_i.V_e*(vector_rho_s*J)
+            -TLGF_v.V_e*eta_1*(eps_1/eps_)*sin(theta_s)*(vector_z_s*J)
             )*factor;
-        fields.E.phi = -j*k_1*(exp(-j*k_1*r)/(2.0*pi*r))*exp(+j*k_1*cos(theta_s)*z_)*(
+        fields.E.phi = -j*2.0*k_1*(exp(-j*k_1*r)/(4.0*pi*r))*exp(+j*k_1*cos(theta_s)*z_)*(
             +TLGF_i.V_h*cos(theta_s)*(vector_phi_s*J)
             )*factor;
         fields.H.theta = -fields.E.phi/eta_1;
-        fields.H.phi = -fields.E.theta/eta_1;
+        fields.H.phi = +fields.E.theta/eta_1;
     }
     if (side==bottom){
         const size_t m=configuration_t::find_layer(dipole_J.r.z);
@@ -1634,14 +1635,15 @@ far_field_t configuration_t::compute_far_field_J(const real_t r, const real_t th
         vector_t<complex_t> J=vector_t<complex_t>(dipole_J.x, dipole_J.y, dipole_J.z);
         const complex_t factor=exp(+j*(vector_k_rho_s.x*dipole_J.r.x+vector_k_rho_s.y*dipole_J.r.y));
         vector_t<complex_t> E;
-        fields.E.theta = -j*k_N*(exp(-j*k_N*r)/(2.0*pi*r))*exp(+j*k_N*cos(theta_s)*z_)*(
-            TLGF_i.V_e*(vector_rho_s*J)-TLGF_v.V_e*eta_N*(eps_N/eps_)*sin(theta_s)*(vector_z_s*J)
+        fields.E.theta = -j*2.0*k_N*(exp(-j*k_N*r)/(4.0*pi*r))*exp(+j*k_N*cos(theta_s)*z_)*(
+            TLGF_i.V_e*(vector_rho_s*J)
+            -TLGF_v.V_e*eta_N*(eps_N/eps_)*sin(theta_s)*(vector_z_s*J)
             )*factor;
-        fields.E.phi = -j*k_N*(exp(-j*k_N*r)/(2.0*pi*r))*exp(+j*k_N*cos(theta_s)*z_)*(
+        fields.E.phi = -j*2.0*k_N*(exp(-j*k_N*r)/(4.0*pi*r))*exp(+j*k_N*cos(theta_s)*z_)*(
             +TLGF_i.V_h*cos(theta_s)*(vector_phi_s*J)
             )*factor;
         fields.H.theta = -fields.E.phi/eta_N;
-        fields.H.phi = -fields.E.theta/eta_N;
+        fields.H.phi = +fields.E.theta/eta_N;
     }
     return fields;
 }
@@ -1676,14 +1678,15 @@ far_field_t configuration_t::compute_far_field_M(const real_t r, const real_t th
         vector_t<complex_t> M=vector_t<complex_t>(dipole_M.x, dipole_M.y, dipole_M.z);
         const complex_t factor=exp(+j*(vector_k_rho_s.x*dipole_M.r.x+vector_k_rho_s.y*dipole_M.r.y));
         vector_t<complex_t> E;
-        fields.E.theta = +j*k_1*(exp(-j*k_1*r)/(2.0*pi*r))*exp(+j*k_1*cos(theta_s)*z_)*(
+        fields.E.theta = +j*2.0*k_1*(exp(-j*k_1*r)/(4.0*pi*r))*exp(+j*k_1*cos(theta_s)*z_)*(
             -TLGF_v.V_e*(vector_phi_s*M)
             )*factor;
-        fields.E.phi = +j*k_1*(exp(-j*k_1*r)/(2.0*pi*r))*exp(+j*k_1*cos(theta_s)*z_)*(
-            TLGF_v.V_h*cos(theta_s)*(vector_rho_s*M)-TLGF_i.V_h*(1.0/eta_1)*(mu_1/mu_)*sin(theta_s)*cos(theta_s)*(vector_z_s*M)
+        fields.E.phi = +j*2.0*k_1*(exp(-j*k_1*r)/(4.0*pi*r))*exp(+j*k_1*cos(theta_s)*z_)*(
+            TLGF_v.V_h*cos(theta_s)*(vector_rho_s*M)
+            -TLGF_i.V_h*(1.0/eta_1)*(mu_1/mu_)*sin(theta_s)*cos(theta_s)*(vector_z_s*M)
             )*factor;
         fields.H.theta = -fields.E.phi/eta_1;
-        fields.H.phi = -fields.E.theta/eta_1;
+        fields.H.phi = +fields.E.theta/eta_1;
     }
     if (side==bottom){
         const size_t m=configuration_t::find_layer(dipole_M.r.z);
@@ -1705,14 +1708,15 @@ far_field_t configuration_t::compute_far_field_M(const real_t r, const real_t th
         vector_t<complex_t> M=vector_t<complex_t>(dipole_M.x, dipole_M.y, dipole_M.z);
         const complex_t factor=exp(+j*(vector_k_rho_s.x*dipole_M.r.x+vector_k_rho_s.y*dipole_M.r.y));
         vector_t<complex_t> E;
-        fields.E.theta = +j*k_N*(exp(-j*k_N*r)/(2.0*pi*r))*exp(+j*k_N*cos(theta_s)*z_)*(
+        fields.E.theta = +j*2.0*k_N*(exp(-j*k_N*r)/(4.0*pi*r))*exp(+j*k_N*cos(theta_s)*z_)*(
             -TLGF_v.V_e*(vector_phi_s*M)
             )*factor;
-        fields.E.phi = +j*k_N*(exp(-j*k_N*r)/(2.0*pi*r))*exp(+j*k_N*cos(theta_s)*z_)*(
-            TLGF_v.V_h*cos(theta_s)*(vector_rho_s*M)-TLGF_i.V_h*(1.0/eta_N)*(mu_N/mu_)*sin(theta_s)*cos(theta_s)*(vector_z_s*M)
+        fields.E.phi = +j*2.0*k_N*(exp(-j*k_N*r)/(4.0*pi*r))*exp(+j*k_N*cos(theta_s)*z_)*(
+            TLGF_v.V_h*cos(theta_s)*(vector_rho_s*M)
+            -TLGF_i.V_h*(1.0/eta_N)*(mu_N/mu_)*sin(theta_s)*cos(theta_s)*(vector_z_s*M)
             )*factor;
         fields.H.theta = -fields.E.phi/eta_N;
-        fields.H.phi = -fields.E.theta/eta_N;
+        fields.H.phi = +fields.E.theta/eta_N;
     }
     return fields;
 }
