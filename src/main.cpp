94ef4339faa5d044139a547778513bfe7e8cb5a1
Mazin M. Mustafa <mazin.mustafa@outlook.com>
2025-07-23 23:04:34 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main(){
     // test_plasmonic_WG_far_field();
     // test_Chew_far_field();
     // test_Chew_far_field_2_elements();
-    // test_DGFs_Gold_Kretschmann_near_far_fields();
+    test_DGFs_Gold_Kretschmann_near_far_fields();
     // test_modal_analysis();
     // test_CIM();
     // test_CIM_layered_media();
